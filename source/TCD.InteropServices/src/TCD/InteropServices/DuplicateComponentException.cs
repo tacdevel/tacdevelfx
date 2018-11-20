b8d3cc6c91c3e43cb0b6d9d9e5a779583182299d
Thomas Corwin <tacorwin@outlook.com>
2018-11-19 23:56:47 -0500

    More work on NativeComponent

---
+++ source/TCD.InteropServices/src/TCD/InteropServices/DuplicateComponentException.cs
@@ -0,0 +1,46 @@
+﻿/***************************************************************************************************
+ * FileName:             DuplicateComponentException.cs
+ * Date:                 20181119
+ * Copyright:            Copyright © 2017-2018 Thomas Corwin, et al. All Rights Reserved.
+ * License:              https://github.com/tacdevel/tcdfx/blob/master/LICENSE.md
+ **************************************************************************************************/
+
+using System;
+using System.Runtime.Serialization;
+
+namespace TCD.InteropServices
+{
+    /// <summary>
+    /// The exception that is thrown when a duplicate handle has been created.
+    /// </summary>
+    public sealed class DuplicateComponentException : SystemException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DuplicateComponentException"/> class.
+        /// </summary>
+        public DuplicateComponentException() : base("") { }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="DuplicateComponentException"/> class
+        ///  with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public DuplicateComponentException(string message) : base(message) { }
+
+        /// <summary>
+        ///  Initializes a new instance of the <see cref="DuplicateComponentException"/> class
+        ///  with a specified error message and a reference to the inner exception that is
+        ///  the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
+        public DuplicateComponentException(string message, Exception innerException) : base(message, innerException) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DuplicateComponentException"/> class with serialized data.
+        /// </summary>
+        /// <param name="info">The object that holds the serialized object data.</param>
+        /// <param name="context">The contextual information about the source or destination.</param>
+        public DuplicateComponentException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
--- source/TCD.InteropServices/src/TCD/InteropServices/NativeComponent.cs
+++ source/TCD.InteropServices/src/TCD/InteropServices/NativeComponent.cs
@@ -16,36 +16,23 @@ namespace TCD.InteropServices
     /// </summary>
     public abstract class NativeComponent : Disposable, IEquatable<NativeComponent>
     {
-        private static List<NativeComponent> componentCache = new List<NativeComponent>();
-        private static List<IntPtr> handleCache = new List<IntPtr>();
-        private readonly int index;
+        private static readonly Dictionary<NativeComponent, IntPtr> cache = new Dictionary<NativeComponent, IntPtr>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NativeComponent"/> class with the specified handle.
         /// </summary>
-        /// <param name="handle"></param>
+        /// <param name="handle">The pre-existing handle for this <see cref="NativeComponent"/></param>
         protected NativeComponent(IntPtr handle)
         {
-            index = componentCache.Count;
+            if (cache.ContainsKey(this)) throw new DuplicateComponentException();
             Handle = handle;
+            cache.Add(this, handle);
         }
 
         /// <summary>
         /// Gets the handle for this <see cref="NativeComponent"/>.
         /// </summary>
-        public IntPtr Handle
-        {
-            get => handleCache[index];
-            protected internal set
-            {
-                if (index > 0)
-                    if (componentCache.Contains(this) || handleCache.Contains(value))
-                        throw new DuplicateHandleException();
-
-                componentCache.Add(this);
-                handleCache.Add(value);
-            }
-        }
+        public IntPtr Handle { get; private set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="NativeComponent"/> is invalid.
@@ -74,7 +61,7 @@ namespace TCD.InteropServices
         /// </summary>
         /// <param name="component">The component to compare with this <see cref="NativeComponent"/>.</param>
         /// <returns>true if obj and this <see cref="NativeComponent"/> represent the same value; otherwise, false.</returns>
-        public bool Equals(NativeComponent component) => index == component.index;
+        public bool Equals(NativeComponent component) => Handle == component.Handle;
 
         /// <summary>
         /// Serves as the default hash function.
@@ -86,7 +73,7 @@ namespace TCD.InteropServices
         /// Returns a string that represents this <see cref="NativeComponent"/>.
         /// </summary>
         /// <returns>A string that represents this <see cref="NativeComponent"/></returns>
-        public override string ToString() => handleCache[index].ToInt64().ToString();
+        public override string ToString() => Handle.ToInt64().ToString();
 
         /// <summary>
         /// Performs tasks associated with releasing unmanaged resources.
@@ -98,12 +85,10 @@ namespace TCD.InteropServices
         /// </summary>
         protected override void ReleaseManagedResources()
         {
-            if (!IsInvalid && componentCache.Contains(this))
-            {
-                componentCache.RemoveAt(index);
-                handleCache[index] = IntPtr.Zero;
-                handleCache.RemoveAt(index);
-            }
+            if (cache.ContainsKey(this))
+                cache.Remove(this);
+            if (!IsInvalid)
+                Handle = IntPtr.Zero;
         }
     }
 
@@ -115,39 +100,23 @@ namespace TCD.InteropServices
     public abstract class NativeComponent<T> : Disposable, IEquatable<NativeComponent<T>>
         where T : SafeHandle
     {
-        private static readonly List<NativeComponent<T>> componentCache = new List<NativeComponent<T>>();
-        private static readonly List<T> handleCache = new List<T>();
-        private readonly int index;
+        private static readonly Dictionary<NativeComponent<T>, T> cache = new Dictionary<NativeComponent<T>, T>();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="NativeComponent{T}"/> class with the specified handle.
         /// </summary>
-        /// <param name="handle"></param>
+        /// <param name="handle">The pre-existing handle for this <see cref="NativeComponent{T}"/></param>
         protected NativeComponent(T handle)
         {
-            index = componentCache.Count;
+            if (cache.ContainsKey(this)) throw new DuplicateComponentException();
             Handle = handle;
+            cache.Add(this, handle);
         }
 
         /// <summary>
         /// Gets the handle for this <see cref="NativeComponent{T}"/>.
         /// </summary>
-        public T Handle
-        {
-            get => handleCache[index];
-            protected internal set
-            {
-                if (index > 0)
-                    if (componentCache.Contains(this) || handleCache.Contains(value))
-                        throw new DuplicateHandleException();
-
-                if (value.IsInvalid || value.IsClosed)
-                    throw new InvalidHandleException();
-
-                componentCache.Add(this);
-                handleCache.Add(value);
-            }
-        }
+        public T Handle { get; private set; }
 
         /// <summary>
         /// Gets a value indicating whether this <see cref="NativeComponent{T}"/> is invalid.
@@ -176,7 +145,7 @@ namespace TCD.InteropServices
         /// </summary>
         /// <param name="component">The component to compare with this <see cref="NativeComponent{T}"/>.</param>
         /// <returns>true if obj and this <see cref="NativeComponent{T}"/> represent the same value; otherwise, false.</returns>
-        public bool Equals(NativeComponent<T> component) => index == component.index;
+        public bool Equals(NativeComponent<T> component) => Handle == component.Handle;
 
         /// <summary>
         /// Serves as the default hash function.
@@ -188,7 +157,7 @@ namespace TCD.InteropServices
         /// Returns a string that represents this <see cref="NativeComponent{T}"/>.
         /// </summary>
         /// <returns>A string that represents this <see cref="NativeComponent{T}"/></returns>
-        public override string ToString() => handleCache[index].DangerousGetHandle().ToInt64().ToString();
+        public override string ToString() => Handle.DangerousGetHandle().ToInt64().ToString();
 
         /// <summary>
         /// Performs tasks associated with releasing unmanaged resources.
@@ -200,12 +169,10 @@ namespace TCD.InteropServices
         /// </summary>
         protected override void ReleaseManagedResources()
         {
-            if (!IsInvalid && componentCache.Contains(this))
-            {
-                componentCache.RemoveAt(index);
-                handleCache[index].Dispose();
-                handleCache.RemoveAt(index);
-            }
+            if (cache.ContainsKey(this))
+                cache.Remove(this);
+            if (!IsInvalid)
+                Handle.Dispose();
         }
     }
 }
