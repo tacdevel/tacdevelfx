6148e1f8d68b89596665089a8e41592e8b7c6fb8
Thomas Corwin <tacorwin@outlook.com>
2019-04-15 23:12:21 -0400

    Added a function in Numerics.Hashing that uses reflection to get props

--- source/TCD.Numerics.Hashing/src/TCD/Numerics/Hashing/ObjectExtensions.cs
+++ source/TCD.Numerics.Hashing/src/TCD/Numerics/Hashing/ObjectExtensions.cs
@@ -1,15 +1,17 @@
 /***************************************************************************************************
  * FileName:             ObjectExtensions.cs
-  * Copyright:            Copyright © 2017-2019 Thomas Corwin, et al. All Rights Reserved.
+ * Copyright:            Copyright © 2017-2019 Thomas Corwin, et al. All Rights Reserved.
  * License:              https://github.com/tom-corwin/tcdfx/blob/master/LICENSE.md
  **************************************************************************************************/
+
+ using System.Reflection;
 
 namespace TCD.Numerics.Hashing
 {
     /// <summary>
     /// Contains extension methods to hash object values.
     /// </summary>
-    public static class HashingExtensions
+    public static class ObjectExtensions
     {
         // See: https://github.com/dotnet/corefx/blob/master/src/Common/src/System/Numerics/Hashing/HashHelpers.cs
         /// <summary>
@@ -33,6 +35,22 @@ namespace TCD.Numerics.Hashing
                 }
                 return hash;
             }
+        }
+
+        /// <summary>
+        /// Generates a hash code by getting the public, instance properties from this object.
+        /// </summary>
+        /// <param name="self">The current object.</param>
+        /// <returns>A generated hash code.</returns>
+        public static int GenerateHashCode(this object self)
+        {
+            PropertyInfo[] propInfos = self.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty);
+            object[] props = new object[propInfos.Length];
+            for (int i = 0; i < propInfos.Length; i++)
+            {
+                props[i] = propInfos[i].GetValue(self);
+            }
+            return GenerateHashCode(self, props);
         }
     }
 }
