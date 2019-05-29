10e9951aa0650df96feda630e17361b14b779833
tom-corwin <tacorwin@outlook.com>
2019-05-28 21:30:16 -0400

    Fixed an issue with the msbuild files.
    Signed-off-by: tom-corwin <tacorwin@outlook.com>

---
+++ sources/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+/***************************************************************************************************
+ * FileName:             AssemblyInfo.cs
+ * Copyright:            Copyright © 2017-2019 Thomas Corwin, et al. All Rights Reserved.
+ * License:              https://github.com/tom-corwin/tcdfx/blob/master/LICENSE.md
+ **************************************************************************************************/
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.CLSCompliant(true)]
