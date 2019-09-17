d211942dbb2eb1a9a09d14c4a7dc94c16929eb98
tom-corwin <tacorwin@outlook.com>
2019-09-17 13:52:10 -0400

    Made everything in Libui.cs internal, also removed ctor.

--- sources/TCDFx.UI/source/TCDFx/Native/Libui.cs
+++ sources/TCDFx.UI/source/TCDFx/Native/Libui.cs
@@ -20,143 +20,133 @@ namespace TCDFx.Native
         #region Helpers
         private const CallingConvention Convention = CallingConvention.Cdecl;
         private const LayoutKind Layout = LayoutKind.Sequential;
-
-        static Libui()
-        {
-            if (Platform.PlatformType == PlatformType.Windows && Platform.Architecture == PlatformArch.X64)
-                AssemblyRef = new NativeAssembly(@"runtimes\win-x64\native\libui.dll");
-            else if (Platform.PlatformType == PlatformType.MacOS && Platform.Architecture == PlatformArch.X64)
-                AssemblyRef = new NativeAssembly(@"runtimes/osx-x64/native/libui.dylib", @"runtimes/osx-x64/native/libui.A.dylib");
-            else if ((Platform.PlatformType == PlatformType.Linux || Platform.PlatformType == PlatformType.FreeBSD) && Platform.Architecture == PlatformArch.X64)
-                AssemblyRef = new NativeAssembly(@"runtimes/linux-x64/native/libui.so", @"runtimes/linux-x64/native/libui.so.0");
-            else
-                throw new PlatformNotSupportedException();
-        }
-
-        private static NativeAssembly AssemblyRef { get; }
-
-        public static T Call<T>() where T : Delegate => AssemblyRef.LoadFunction<T>();
+        private static readonly NativeAssembly AssemblyRef =
+            (Platform.IsWindows && Platform.Is32Bit) ? new NativeAssembly(@"runtimes\win-x86\native\libui.dll") :
+            (Platform.IsWindows && Platform.Is64Bit) ? new NativeAssembly(@"runtimes\win-x64\native\libui.dll") :
+            (Platform.IsMacOS && Platform.Is64Bit) ? new NativeAssembly(@"runtimes/osx-x64/native/libui.dylib", @"runtimes/osx-x64/native/libui.A.dylib") :
+            ((Platform.IsLinux || Platform.IsFreeBSD) && Platform.Is64Bit) ? new NativeAssembly(@"runtimes/linux-x64/native/libui.so", @"runtimes/linux-x64/native/libui.so.0") :
+            throw new PlatformNotSupportedException();
         #endregion
 
         [StructLayout(Layout)]
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public struct uiInitError
+        internal struct uiInitError
         {
-            public UIntPtr Size;
-            public char[] Message;
+            internal UIntPtr Size;
+            internal char[] Message;
         }
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static bool uiInit(IntPtr options, out uiInitError err) => Call<FunctionPointers.uiInit>()(options, out err);
+        internal static bool uiInit(IntPtr options, out uiInitError err) => AssemblyRef.LoadFunction<FunctionPointers.uiInit>()(options, out err);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiMain() => Call<FunctionPointers.uiMain>()();
+        internal static void uiMain() => AssemblyRef.LoadFunction<FunctionPointers.uiMain>()();
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiQuit() => Call<FunctionPointers.uiQuit>()();
+        internal static void uiQuit() => AssemblyRef.LoadFunction<FunctionPointers.uiQuit>()();
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiQueueMain(uiQueueMainFuncPtr f, IntPtr data) => Call<FunctionPointers.uiQueueMain>()(f, data);
+        internal static void uiQueueMain(uiQueueMainFuncPtr f, IntPtr data) => AssemblyRef.LoadFunction<FunctionPointers.uiQueueMain>()(f, data);
 
         [UnmanagedFunctionPointer(Convention)]
-        public delegate void uiQueueMainFuncPtr(IntPtr data);
+        internal delegate void uiQueueMainFuncPtr(IntPtr data);
 
         [UnmanagedFunctionPointer(Convention)]
-        public delegate void uiEventHandler(IntPtr sender, IntPtr args, IntPtr data);
+        internal delegate void uiEventHandler(IntPtr sender, IntPtr args, IntPtr data);
 
         [StructLayout(Layout)]
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public struct uiEventOptions
+        internal struct uiEventOptions
         {
-            public UIntPtr Size;
-            public bool Global;
+            internal UIntPtr Size;
+            internal bool Global;
         }
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static IntPtr uiNewEvent(uiEventOptions options) => Call<FunctionPointers.uiNewEvent>()(options);
+        internal static IntPtr uiNewEvent(uiEventOptions options) => AssemblyRef.LoadFunction<FunctionPointers.uiNewEvent>()(options);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiEventFree(IntPtr e) => Call<FunctionPointers.uiEventFree>()(e);
+        internal static void uiEventFree(IntPtr e) => AssemblyRef.LoadFunction<FunctionPointers.uiEventFree>()(e);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static int uiEventAddHandler(IntPtr e, uiEventHandler handler, IntPtr sender, IntPtr data) => Call<FunctionPointers.uiEventAddHandler>()(e, handler, sender, data);
+        internal static int uiEventAddHandler(IntPtr e, uiEventHandler handler, IntPtr sender, IntPtr data) => AssemblyRef.LoadFunction<FunctionPointers.uiEventAddHandler>()(e, handler, sender, data);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiEventDeleteHandler(IntPtr e, int id) => Call<FunctionPointers.uiEventDeleteHandler>()(e, id);
+        internal static void uiEventDeleteHandler(IntPtr e, int id) => AssemblyRef.LoadFunction<FunctionPointers.uiEventDeleteHandler>()(e, id);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiEventFire(IntPtr e, IntPtr sender, IntPtr args) => Call<FunctionPointers.uiEventFire>()(e, sender, args);
+        internal static void uiEventFire(IntPtr e, IntPtr sender, IntPtr args) => AssemblyRef.LoadFunction<FunctionPointers.uiEventFire>()(e, sender, args);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static bool uiEventHandlerBlocked(IntPtr e, int id) => Call<FunctionPointers.uiEventHandlerBlocked>()(e, id);
+        internal static bool uiEventHandlerBlocked(IntPtr e, int id) => AssemblyRef.LoadFunction<FunctionPointers.uiEventHandlerBlocked>()(e, id);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiEventSetHandlerBlocked(IntPtr e, int id, bool blocked) => Call<FunctionPointers.uiEventSetHandlerBlocked>()(e, id, blocked);
+        internal static void uiEventSetHandlerBlocked(IntPtr e, int id, bool blocked) => AssemblyRef.LoadFunction<FunctionPointers.uiEventSetHandlerBlocked>()(e, id, blocked);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiEventInvalidateSender(IntPtr e, IntPtr sender) => Call<FunctionPointers.uiEventInvalidateSender>()(e, sender);
+        internal static void uiEventInvalidateSender(IntPtr e, IntPtr sender) => AssemblyRef.LoadFunction<FunctionPointers.uiEventInvalidateSender>()(e, sender);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static uint uiControType() => Call<FunctionPointers.uiControType>()();
+        internal static uint uiControType() => AssemblyRef.LoadFunction<FunctionPointers.uiControType>()();
 
         [StructLayout(Layout)]
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public struct uiControlVTable
+        internal struct uiControlVTable
         {
-            public UIntPtr Size;
-            public uiControlVTableInitFuncPtr Init;
-            public uiControlVTableFreeFuncPtr Free;
+            internal UIntPtr Size;
+            internal uiControlVTableInitFuncPtr Init;
+            internal uiControlVTableFreeFuncPtr Free;
         }
 
         [UnmanagedFunctionPointer(Convention)]
-        public delegate bool uiControlVTableInitFuncPtr(IntPtr c, IntPtr implData, IntPtr initData);
+        internal delegate bool uiControlVTableInitFuncPtr(IntPtr c, IntPtr implData, IntPtr initData);
 
         [UnmanagedFunctionPointer(Convention)]
-        public delegate void uiControlVTableFreeFuncPtr(IntPtr c, IntPtr implData);
+        internal delegate void uiControlVTableFreeFuncPtr(IntPtr c, IntPtr implData);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static uint uiRegisterControlType(string name, uiControlVTable vtable, IntPtr osVtable, UIntPtr implDataSize) => Call<FunctionPointers.uiRegisterControlType>()(name, vtable, osVtable, implDataSize);
+        internal static uint uiRegisterControlType(string name, uiControlVTable vtable, IntPtr osVtable, UIntPtr implDataSize) => AssemblyRef.LoadFunction<FunctionPointers.uiRegisterControlType>()(name, vtable, osVtable, implDataSize);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static IntPtr uiCheckControlType(IntPtr c, uint type) => Call<FunctionPointers.uiCheckControlType>()(c, type);
+        internal static IntPtr uiCheckControlType(IntPtr c, uint type) => AssemblyRef.LoadFunction<FunctionPointers.uiCheckControlType>()(c, type);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static IntPtr uiNewControl(uint type, IntPtr initData) => Call<FunctionPointers.uiNewControl>()(type, initData);
+        internal static IntPtr uiNewControl(uint type, IntPtr initData) => AssemblyRef.LoadFunction<FunctionPointers.uiNewControl>()(type, initData);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiControlFree(IntPtr c) => Call<FunctionPointers.uiControlFree>()(c);
+        internal static void uiControlFree(IntPtr c) => AssemblyRef.LoadFunction<FunctionPointers.uiControlFree>()(c);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static void uiControlSetParent(IntPtr c, IntPtr parent) => Call<FunctionPointers.uiControlSetParent>()(c, parent);
+        internal static void uiControlSetParent(IntPtr c, IntPtr parent) => AssemblyRef.LoadFunction<FunctionPointers.uiControlSetParent>()(c, parent);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static IntPtr uiControlImplData(IntPtr c) => Call<FunctionPointers.uiControlImplData>()(c);
+        internal static IntPtr uiControlImplData(IntPtr c) => AssemblyRef.LoadFunction<FunctionPointers.uiControlImplData>()(c);
 
         [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "NativeMemberName")]
-        public static IntPtr uiControlOnFree() => Call<FunctionPointers.uiControlOnFree>()();
+        internal static IntPtr uiControlOnFree() => AssemblyRef.LoadFunction<FunctionPointers.uiControlOnFree>()();
 
         private static class FunctionPointers
         {
-            [UnmanagedFunctionPointer(Convention)] public delegate bool uiInit(IntPtr options, out uiInitError err);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiMain();
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiQuit();
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiQueueMain(uiQueueMainFuncPtr f, IntPtr data);
-            [UnmanagedFunctionPointer(Convention)] public delegate IntPtr uiNewEvent(uiEventOptions options);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiEventFree(IntPtr e);
-            [UnmanagedFunctionPointer(Convention)] public delegate int uiEventAddHandler(IntPtr e, uiEventHandler handler, IntPtr sender, IntPtr data);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiEventDeleteHandler(IntPtr e, int id);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiEventFire(IntPtr e, IntPtr sender, IntPtr args);
-            [UnmanagedFunctionPointer(Convention)] public delegate bool uiEventHandlerBlocked(IntPtr e, int id);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiEventSetHandlerBlocked(IntPtr e, int id, bool blocked);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiEventInvalidateSender(IntPtr e, IntPtr sender);
-            [UnmanagedFunctionPointer(Convention)] public delegate uint uiControType();
-            [UnmanagedFunctionPointer(Convention)] public delegate uint uiRegisterControlType(string name, uiControlVTable vtable, IntPtr osVtable, UIntPtr implDataSize);
-            [UnmanagedFunctionPointer(Convention)] public delegate IntPtr uiCheckControlType(IntPtr c, uint type);
-            [UnmanagedFunctionPointer(Convention)] public delegate IntPtr uiNewControl(uint type, IntPtr initData);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiControlFree(IntPtr c);
-            [UnmanagedFunctionPointer(Convention)] public delegate void uiControlSetParent(IntPtr c, IntPtr parent);
-            [UnmanagedFunctionPointer(Convention)] public delegate IntPtr uiControlImplData(IntPtr c);
-            [UnmanagedFunctionPointer(Convention)] public delegate IntPtr uiControlOnFree();
+            [UnmanagedFunctionPointer(Convention)] internal delegate bool uiInit(IntPtr options, out uiInitError err);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiMain();
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiQuit();
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiQueueMain(uiQueueMainFuncPtr f, IntPtr data);
+            [UnmanagedFunctionPointer(Convention)] internal delegate IntPtr uiNewEvent(uiEventOptions options);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiEventFree(IntPtr e);
+            [UnmanagedFunctionPointer(Convention)] internal delegate int uiEventAddHandler(IntPtr e, uiEventHandler handler, IntPtr sender, IntPtr data);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiEventDeleteHandler(IntPtr e, int id);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiEventFire(IntPtr e, IntPtr sender, IntPtr args);
+            [UnmanagedFunctionPointer(Convention)] internal delegate bool uiEventHandlerBlocked(IntPtr e, int id);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiEventSetHandlerBlocked(IntPtr e, int id, bool blocked);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiEventInvalidateSender(IntPtr e, IntPtr sender);
+            [UnmanagedFunctionPointer(Convention)] internal delegate uint uiControType();
+            [UnmanagedFunctionPointer(Convention)] internal delegate uint uiRegisterControlType(string name, uiControlVTable vtable, IntPtr osVtable, UIntPtr implDataSize);
+            [UnmanagedFunctionPointer(Convention)] internal delegate IntPtr uiCheckControlType(IntPtr c, uint type);
+            [UnmanagedFunctionPointer(Convention)] internal delegate IntPtr uiNewControl(uint type, IntPtr initData);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiControlFree(IntPtr c);
+            [UnmanagedFunctionPointer(Convention)] internal delegate void uiControlSetParent(IntPtr c, IntPtr parent);
+            [UnmanagedFunctionPointer(Convention)] internal delegate IntPtr uiControlImplData(IntPtr c);
+            [UnmanagedFunctionPointer(Convention)] internal delegate IntPtr uiControlOnFree();
         }
     }
 }
