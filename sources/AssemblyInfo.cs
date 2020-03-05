fd1ce446fe393bce581c7a34426913bba0a613dd
Thomas Corwin <tacorwin@outlook.com>
2020-03-05 02:04:31 -0500

    Updated the MSBuild (not .csproj) files, and updated global.json

---
+++ sources/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+/***********************************************************************************************************************
+ * FileName:             /sources/AssemblyInfo.cs
+ * Copyright/License:    https://github.com/tom-corwin/tacdevlibs/blob/master/LICENSE.md
+***********************************************************************************************************************/
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.CLSCompliant(true)]
