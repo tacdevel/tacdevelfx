c73cc7e56a1cb5297304a4ffedaa0054e48f3a9d
Thomas Corwin <tacorwin@outlook.com>
2020-03-06 00:44:12 -0500

    Added some of the old examples and MSBuild files for future tests.

---
+++ examples/MultiValueDictionaryExample/source/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.InteropServices;
+using TACDevel.Runtime.InteropServices;
+
+namespace NativeAssemblyExample
+{
+    internal class Program
+    {
+        public static void Main() { }
+    }
+}
