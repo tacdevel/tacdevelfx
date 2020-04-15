acade059dc51df7175ec3bb0920b9a85bc7e5df1
Thomas Corwin <tacorwin@outlook.com>
2020-04-15 13:43:02 -0400

    Added private helpers to TACDevel.Native.Miniaudio in TACDevel.Audio

--- sources/TACDevel.Audio/src/TACDevel/Native/Miniaudio.cs
+++ sources/TACDevel.Audio/src/TACDevel/Native/Miniaudio.cs
@@ -6,13 +6,23 @@
 using System;
 using System.Runtime.InteropServices;
 using System.Security;
+using TACDevel.Runtime;
+using TACDevel.Runtime.InteropServices;
 
 namespace TACDevel.Native
 {
     [SuppressUnmanagedCodeSecurity]
     internal static class Miniaudio
     {
-        private const string AssemblyRef = "miniaudio";
+        private const CallingConvention Convention = CallingConvention.Cdecl;
+        private const LayoutKind Layout = LayoutKind.Sequential;
+        private static readonly NativeAssembly asm = new NativeAssembly(
+            (Platform.Is64Bit && Platform.IsWindows) ? @"runtimes\win-x64\miniaudio.dll" :
+            (Platform.Is64Bit && Platform.IsMacOS) ? @"runtimes\osx-x64\miniaudio.dylib" :
+            (Platform.Is64Bit && Platform.IsLinux) ? @"runtimes\linux-x64\miniaudio.so" :
+            throw new PlatformNotSupportedException());
+        private static T Call<T>() where T : Delegate => asm.LoadFunction<T>();
+
 
     }
 }
